b92c0d2cd13e791e3dec5695a046d87ee7b21fc6
Amr Khaled <amro.amor1234@gmail.com>
2024-06-22 22:57:07 +0300

    Enumeration Constants(enum class)

--- chapter06/GameofChance.cpp
+++ chapter06/GameofChance.cpp
@@ -12,45 +12,46 @@ int main(){
     
     srand(time(0)); // Generate different result every run
 
-    string gameStatus;
+    enum class Status{CONTINUE,LOST,WON}; // (0,1,2)our constanat class
+    Status gameStatus;
     int diceSum{dicePlay()},myPoint{0}; // now this is sum our first roll
 
     switch (diceSum) //check for that sum
     {
     case 7:
     case 11:  // 11,7 will make win
-        gameStatus = "Win";  
+        gameStatus = Status::WON;  
         break;
     case 2:
     case 3:
     case 12: //2,3,12 will make you lose
-        gameStatus = "Lose";
+        gameStatus = Status::LOST;
         break;
 
     default: // other numbers will continue the game to win you have to get same sum again
-        gameStatus = "Continue";
+        gameStatus = Status::CONTINUE;
         myPoint = diceSum; // here storing it
         cout << "Your Point is: "<<myPoint<<endl; //printing to screen
         break;
     }
 
-    while (gameStatus== "Continue"){ // if we continue we have to do it until win or lose
+    while (gameStatus== Status::CONTINUE){ // if we continue we have to do it until win or lose
         diceSum = dicePlay();
         if(diceSum==myPoint){ // you win if your old sum is the current play
-            gameStatus= "Win";
+            gameStatus= Status::WON;
 
         }
         else{ // 
             if(diceSum==7){ // you lost if 7
-                gameStatus= "Lose";
+                gameStatus= Status::LOST;
             }
         }
 
     }
     // here we have gamestatus is either lose or win 
-    if (gameStatus=="Win"){ //this don't need explaination
+    if (gameStatus==Status::WON){ //this don't need explaination
         cout << "You Won!";
-    }else if (gameStatus=="Lose"){
+    }else if (gameStatus==Status::LOST){
         cout << "You Lost!";
 
     }
